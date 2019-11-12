1bbc81f16654a76a33af8b90507ba4fbe358add2
bieniu <maciej.bieniek@gmail.com>
2019-11-12 11:20:01 +0100

    Python version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_namespace_packages, setup
 
 setup(
     name="gios",
-    version="0.0.2",
+    version="0.0.3",
     author="Maciej Bieniek",
     author_email="maciej.bieniek@gmail.com",
     description="Python wrapper for getting air quality data from GIOŚ servers.",
@@ -12,12 +12,14 @@ setup(
     url="https://github.com/bieniu/gios",
     license="Apache 2",
     packages=["gios"],
-    python_requires=">=3.7.0",
+    python_requires=">=3.6.0",
     install_requires=["aiohttp"],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
     ],
     setup_requires=("pytest-runner"),
