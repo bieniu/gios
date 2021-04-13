c963e37117e67e5e7f66a2feda3d1120adfeb98a
Maciej Bieniek <bieniu@users.noreply.github.com>
2021-04-13 09:20:48 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 from setuptools import setup
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="gios",
     version="0.2.1",
     author="Maciej Bieniek",
-    author_email="maciej.bieniek@gmail.com",
     description="Python wrapper for getting air quality data from GIOŚ servers.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     include_package_data=True,
     url="https://github.com/bieniu/gios",
     license="Apache-2.0 License",
