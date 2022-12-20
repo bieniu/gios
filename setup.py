cec50815d547ff54e12b79b2fb328511be9ddf68
Maciej Bieniek <bieniu@users.noreply.github.com>
2022-12-20 11:13:40 +0100

    Use Tox for tests (#94)
    * Use tox for tests
    
    * Update license
    
    * Update gh actions
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
-#!/usr/bin/env python3
+"""Setup module for gios."""
+from pathlib import Path
 from setuptools import setup
 
-with open("README.md", encoding="utf-8") as file:
-    long_description = file.read()
-
-with open("requirements.txt", encoding="utf-8") as file:
-    install_requires = list(val.strip() for val in file.readlines())
-
-with open("requirements-test.txt", encoding="utf-8") as file:
-    tests_require = list(val.strip() for val in file.readlines())
+PROJECT_DIR = Path(__file__).parent.resolve()
+README_FILE = PROJECT_DIR / "README.md"
+VERSION = "2.3.0"
 
 setup(
     name="gios",
-    version="2.2.0",
+    version=VERSION,
     author="Maciej Bieniek",
     description="Python wrapper for getting air quality data from GIOŚ servers.",
-    long_description=long_description,
+    long_description=README_FILE.read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     include_package_data=True,
     url="https://github.com/bieniu/gios",
@@ -23,19 +19,19 @@ setup(
     packages=["gios"],
     package_data={"gios": ["py.typed"]},
     python_requires=">=3.9",
-    install_requires=install_requires,
+    install_requires=list(
+        val.strip() for val in open("requirements.txt", encoding="utf-8")
+    ),
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3",     
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3 :: Only",
         "Typing :: Typed",
     ],
-    setup_requires=("pytest-runner"),
-    tests_require=tests_require,
 )
