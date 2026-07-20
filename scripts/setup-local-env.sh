562271711307bc4f119598bb84ec0efd28665c61
Maciej Bieniek <bieniu@users.noreply.github.com>
2026-07-20 15:27:23 +0200

    Fix script

--- scripts/setup-local-env.sh
+++ scripts/setup-local-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if ! command -v uv >/dev/null 2>&1; then
+	curl -LsSf https://astral.sh/uv/install.sh | sh
+	export PATH="$HOME/.local/bin:$HOME/.cargo/bin:$PATH"
+fi
+
 if command -v python3.14 >/dev/null 2>&1; then
 	PYTHON_VERSION=3.14
 elif command -v python3.13 >/dev/null 2>&1; then
@@ -9,6 +14,5 @@ else
 	exit 1
 fi
 
-python$PYTHON_VERSION -m pip install uv --upgrade
 uv sync --frozen --all-groups --python=$PYTHON_VERSION
 uv run prek install
